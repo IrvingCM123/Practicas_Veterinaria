ecf6ee4b40a59874c09c546087caf33ab4625084
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-09-17 15:31:37 -0600

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { EscanerAdapter } from './config/adaptadores/escaner-adapter/escaner-ada
 import { CommonModule } from '@angular/common';
 import { GenerarCodigoBarrasComponent } from './ui/generar-codigo-barras/generar-codigo-barras.component';
 import { ImprimirTicketsComponent } from './ui/imprimir-tickets/imprimir-tickets.component';
+import { HeaderComponent } from './ui/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
     EscanerComponent,
     GenerarCodigoBarrasComponent,
     ImprimirTicketsComponent,
+    HeaderComponent,
 
   ],
   imports: [
